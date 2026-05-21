7aa80213f3b4461ea1004d3621e608cc91718158
Jonibro123 <jonas.stocker@student.htl-rankweil.at>
2026-05-21 19:17:42 +0200

    Animation

--- game/main.py
+++ game/main.py
@@ -1,89 +1,105 @@
 import pygame
 from game_variables.game_variables import GameVariables as gv
 from game_variables.game_variables import GameScreens as GameScreens
-from objects.sprites import Bilder as B, Bilder
-
+from objects.sprites import Bilder
 
 def main_screen(screen: pygame.Surface, clock: pygame.time.Clock):
     pygame.display.set_caption("Main Screen")
+    frame_counter = 0
 
     titel_text = gv.FONT_BIG.render("Räuber", True, "white")
     starten_text = gv.FONT_MIDDLE.render("Starten", True, "white")
     titel_text_rect = titel_text.get_rect(center=(gv.SCREEN_WIDTH / 2, 100))
-    starten_text_rect = starten_text.get_rect(center=(gv.SCREEN_WIDTH / 2, 250))
-    Geldsack = Bilder("Bilder/Geldsack.png", 1, pygame.Rect(0, 0, 48, 48), 1)
-    Geldsack.load_spritesheet()
-    groesse = (400,400)
-    Geldsack.images[0] = pygame.transform.smoothscale(Geldsack.images[0], groesse)
+    starten_text_rect = starten_text.get_rect(center=(gv.SCREEN_WIDTH / 2, gv.SCREEN_HIGHT / 2))
 
+    Geldsack = Bilder("Bilder/Geldsack.png", 1, pygame.Rect(0, 0, 42, 42), 1)
+    Hintergrund = Bilder("Bilder/Hintergrund.png", 16, pygame.Rect(0, 0, 1024, 1024), 10)
+    Geldsack.load_spritesheet()
+    Hintergrund.load_spritesheet()
+
+    groesse = (400, 400)
+    groesse_hintergrund = (gv.SCREEN_WIDTH, gv.SCREEN_HIGHT)
+
+    Geldsack.images[0] = pygame.transform.smoothscale(Geldsack.images[0], groesse)
+    original_hintergrund = Hintergrund.images[:]
+    Hintergrund.images = [pygame.transform.smoothscale(img, groesse_hintergrund) for img in original_hintergrund]
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return GameScreens.EXIT
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    return GameScreens.EXIT
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                return GameScreens.EXIT
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 if starten_text_rect.collidepoint(event.pos):
                     return GameScreens.ANIMATION
 
-
-
-        screen.fill((14, 0, 108))
+        Hintergrund.draw(screen, 0, 0, frame_counter)
         screen.blit(titel_text, titel_text_rect)
-        Geldsack.draw(screen, gv.SCREEN_WIDTH/2-groesse[0]/2+25,gv.SCREEN_HIGHT/2-groesse[0]/2-25, 1)
+        Geldsack.draw(
+            screen,
+            gv.SCREEN_WIDTH / 2 - groesse[0] / 2,
+            gv.SCREEN_HIGHT / 2 - groesse[1] / 2 + 10,
+            frame_counter
+        )
         screen.blit(starten_text, starten_text_rect)
 
+        frame_counter += 1
         pygame.display.flip()
         clock.tick(gv.FPS)
 
-
 def play_screen(screen: pygame.Surface, clock: pygame.time.Clock):
-    pass
+    return GameScreens.EXIT
 
 def animation_screen(screen: pygame.Surface, clock: pygame.time.Clock):
-    screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Animation")
+    frame_counter = 0
 
-    clock = pygame.time.Clock()
-    running = True
-
-    Geldsack = Bilder("Bilder/Geldsack.png", 1, pygame.Rect(0, 0, 48, 48), 1)
+    Geldsack = Bilder("Bilder/Geldsack.png", 1, pygame.Rect(0, 0, 42, 42), 1)
+    Hintergrund = Bilder("Bilder/Hintergrund.png", 16, pygame.Rect(0, 0, 1024, 1024), 2)
     Geldsack.load_spritesheet()
-    groesse = (400,400)
-    Geldsack.images[0] = pygame.transform.smoothscale(Geldsack.images[0], groesse)
-    Geldsack.draw(screen, gv.SCREEN_WIDTH / 2 - groesse[0] / 2 + 25, gv.SCREEN_HIGHT / 2 - groesse[0] / 2 - 25, 1)
+    Hintergrund.load_spritesheet()
 
-    original = Geldsack.images[0]
+    groesse = (400, 400)
+    groesse_hintergrund = (gv.SCREEN_WIDTH, gv.SCREEN_HIGHT)
 
-    while running:
+    original_geldsack = Geldsack.images[0]
+    original_hintergrund = Hintergrund.images[:]
+    Hintergrund.images = [pygame.transform.smoothscale(img, groesse_hintergrund) for img in original_hintergrund]
+    Geldsack.images[0] = pygame.transform.smoothscale(original_geldsack, groesse)
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                return GameScreens.EXIT
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                return GameScreens.EXIT
 
         screen.fill("black")
-        Geldsack.draw(screen, gv.SCREEN_WIDTH / 2 - groesse[0] / 2 + 25, gv.SCREEN_HIGHT / 2 - groesse[0] / 2 - 25,1)
-        groesse_neu = (groesse[0] + 10, groesse[1] + 10)
-        groesse = groesse_neu
-        Geldsack.images[0] = pygame.transform.smoothscale(original, groesse)
+        Hintergrund.draw(screen, 0, 0, frame_counter)
 
-        if groesse[0] > 10000:
+        Geldsack.draw(
+            screen,
+            gv.SCREEN_WIDTH / 2 - groesse[0] / 2,
+            gv.SCREEN_HIGHT / 2 - groesse[1] / 2 + 10,
+            frame_counter
+        )
+
+        groesse = (groesse[0] + 50, groesse[1] + 50)
+        Geldsack.images[0] = pygame.transform.smoothscale(original_geldsack, groesse)
+
+        frame_counter += 1
+
+        if groesse[0] > gv.SCREEN_WIDTH * 3:
             return GameScreens.PLAY
 
         pygame.display.flip()
         clock.tick(60)
 
-    pygame.quit()
-
-
 def main():
     gv.init()
-    screen = pygame.display.set_mode(
-        (gv.SCREEN_WIDTH, gv.SCREEN_HIGHT)
-    )
+    screen = pygame.display.set_mode((gv.SCREEN_WIDTH, gv.SCREEN_HIGHT))
     clock = pygame.time.Clock()
-
 
     while True:
         if GameScreens.actual == GameScreens.MAIN:
@@ -95,11 +111,7 @@ def main():
         elif GameScreens.actual == GameScreens.EXIT:
             break
 
-
-
-
     pygame.quit()
-
 
 if __name__ == "__main__":
     main()
--- game/objects/sprites.py
+++ game/objects/sprites.py
@@ -10,23 +10,16 @@ class Bilder:
 
     def load_spritesheet(self):
         spritesheet = pygame.image.load(self.filepath).convert_alpha()
-
         for image_index in range(self.image_count):
-            # Neues Surface mit Transparenz
             image_surface = pygame.Surface(self.image_rect.size, pygame.SRCALPHA).convert_alpha()
-
-
             area = pygame.Rect(
                 image_index * self.image_rect.width,
                 0,
                 self.image_rect.width,
                 self.image_rect.height
             )
-
             image_surface.blit(spritesheet, (0, 0), area)
             self.images.append(image_surface)
 
     def draw(self, screen: pygame.Surface, xpos: float, ypos: float, frame_counter: int):
-        index = (frame_counter // self.animation_speed) % self.image_count
-
-        screen.blit(self.images[index], (int(xpos), int(ypos)), )
+        screen.blit(self.images[(frame_counter // self.animation_speed) % self.image_count],(int(xpos), int(ypos)))
